1c9cae06a7f4b76870656a424aad3a9ebdc1bd0d
AbSattar <mdabdullsattar@gmail.com>
2023-09-16 02:24:52 +0530

    modified deploy.js to integrate with swisstronik

--- contracts/SwissCastle.sol
+++ contracts/SwissCastle.sol
@@ -1,5 +1,24 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
 
-contract 
+// importing ERC20 contract from Openzeppelin
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SwissCastle is ERC20 {
+
+    constructor() ERC20("Swisscastle Token", "SWC") {
+        _mint(msg.sender, 100);
+
+
+    }
+    
+}
+
+
+
+
+
+
+
+
